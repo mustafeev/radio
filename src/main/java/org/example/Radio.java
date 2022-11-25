96a075da185b04e1a02097530ea5d7fc246dfbcf
Vadim Mustafeev <ya.mystafeev@yandex.ru>
2022-11-25 20:31:15 +0400

    secondary commit

--- src/main/java/org/example/Radio.java
+++ src/main/java/org/example/Radio.java
@@ -1,11 +1,53 @@
 package org.example;
 
 public class Radio {
+    private int currentStation;
+    private int currentVolume;
 
-    public int currentVolume;
+    public void next() {
+        if (currentStation < 9) {
+            currentStation++;
+        } else {
+            currentStation = 0;
+        }
+    }
 
-    public void currentVolume() {
-        int currentVolume = 0;
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        this.currentStation = currentStation;
+        if (currentStation < 0) {
+            return;
+        }
+        if (currentStation > 9) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
+        }
+        if (currentVolume > 10) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+    public void prev () {
+        if (currentStation > 0) {
+            currentStation --;
+        } else {
+            currentStation = 9;
+        }
+    }
+    public void increaseVolume() {
         if (currentVolume < 10) {
             currentVolume = currentVolume + 1;
         }
--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -1,31 +1,30 @@
 import org.example.Radio;
 import org.junit.jupiter.api.Assertions;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 
 public class RadioTest {
-    private int currentVolume;
-
     @Test
-    public void shouldSetVolume() {
-        Radio cond = new Radio();
-        cond.currentVolume = 2;
-        int expected = 2;
-        int actual = cond.currentVolume;
+    public void test() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
 
+        radio.next();
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
         Assertions.assertEquals(expected, actual);
-
     }
-
     @Test
-    public void setCurrentVolume(int newVolume) {
-        if (newVolume < 0) {
-            return;
-        }
-        if (newVolume > 10) {
-            return;
-        }
-        currentVolume = newVolume;
-    }
-}
+    public void prev() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
 
+        radio.prev();
+
+        int expected = 9;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
 
